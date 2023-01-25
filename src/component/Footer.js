06db8cf6ad768b58fc31ee653afe191f70dd56b1
priyaa74 <priyavarshney070497@gmail.com>
2023-01-25 08:43:52 +0530

    links open in new tab

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -7,20 +7,20 @@ function Footer(){
     return (
         <div className="bg-black text-center">
         <div className="inline-flex space-x-4 justify-center text-center h-20 w-full bg-black mt-4">
-            <a href="https://github.com/priyaa74">
+            <a href="https://github.com/priyaa74" target="_blank">
                 <img alt="github"
                 src="./images.png"
                 className="w-6 h-8 rounded"/>
                 GitHub
             </a>
-            <a href="https://www.linkedin.com/in/priya-varshney-7b9b551b1/">
+            <a href="https://www.linkedin.com/in/07-priya-varshney/" target="_blank">
                 <img alt="linked"
                 src="./linkedIn.png"
                 className="w-6 h-8 rounded"
                 />
                 LinkedIn
             </a>
-            <a href="https://twitter.com/PriyaVa22310248">
+            <a href="https://twitter.com/PriyaVa22310248" target="_blank">
                 <img alt="twitter"
                 src="./twitter.png"
                 className="w-6 h-8"
